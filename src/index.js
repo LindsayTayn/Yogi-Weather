e6abdf7a755386a439156becafffc3142d77ba12
LindsayTayenne <154006361+LindsayTayn@users.noreply.github.com>
2024-01-05 16:51:21 +0100

    Replaced demi content with real life forecast content

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ function refreshWeather(response) {
   timeElement.innerHTML = formatDate(date);
   temperatureElement.innerHTML = Math.round(temperature);
   console.log(response.data);
+  getForecast(response.data.city);
 }
 function formatDate(date) {
   let minutes = date.getMinutes();
@@ -49,33 +50,45 @@ function handleSearchSubmit(event) {
   let searchInput = document.querySelector("#search-form-input");
   searchCity(searchInput.value);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+function getForecast(city) {
+  let apiKey = "90ta40de2b3f7bocf6acde793ab3484a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
   let forecastHtml = "";
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
      <div class="row">
        <div class="col-2">
-        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
         <img
-        src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/mist-night.png"
-        width="36"
+        src="${day.condition.icon_url}"
+        width="40"
         />
        <div class="weather-forecast-temperatures">
-        <span class="weather-forecast-temperature-min">12° </span>
-        <span class="weather-forecast-temperature-max">18°</span>
+        <span class="weather-forecast-temperature-min">
+        ${Math.round(day.temperature.minimum)}° </span>
+        <span class="weather-forecast-temperature-max">
+        ${Math.round(day.temperature.maximum)}°</span>
        </div>
      </div>
     </div>
    `;
+    }
   });
   forecastElement.innerHTML = forecastHtml;
 }
-displayForecast();
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
